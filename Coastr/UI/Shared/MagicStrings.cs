7816c0b26d13d27c57f20ea169fccbb6208b5695
Christian Boland <cboland@outlook.com>
2024-07-14 16:57:01 +0200

    Major Bugfix in MenuItemEdit

--- Coastr/UI/Shared/MagicStrings.cs
+++ Coastr/UI/Shared/MagicStrings.cs
@@ -8,7 +8,12 @@ namespace Coastr.UI.Shared
 {
     public class MagicStrings
     {
-        public static readonly string SECTION_APP_HEADER_NAME = "AppHeaderButton";
+        public static readonly string SECTION_APP_HEADER_TEXT = "AppHeaderText";
+
+        public static readonly string SECTION_APP_HEADER_BUTTON = "AppHeaderButton";
+
+        public const string URL_VENUE_EDIT = "editVenue";
+        public const string URL_VENUE_EDIT_FULL = "/"+ URL_VENUE_EDIT + "/{VenueId:int}";
 
     }
 }
